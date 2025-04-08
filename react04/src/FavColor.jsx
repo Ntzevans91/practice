0f5671656e4f870a66f9fe02c27a542a88cc8520
ntzevans91 <ntzevans91@gmail.com>
2025-04-07 20:18:34 -0500

    104

--- react04/src/FavColor.jsx
+++ react04/src/FavColor.jsx
@@ -1,14 +1,24 @@
 import React, {useState} from 'react'
 
+const styles = {
+
+    btn1:{backgroundColor: "blue", margin:"5px"},
+    btn2:{backgroundColor: "red", margin:"5px"},
+    btn3:{backgroundColor: "green", margin:"5px"},
+  
+};
+   
+
+
 function FavColor(){
     const [color, setColor]= useState("");
 
     return(
         <>
         <h1>My favorite color is <span style={{color}}>{color}</span>.</h1>
-        <button type="button" onClick={()=>setColor("blue")}>blue</button>
-        <button type="button" onClick={()=>setColor("red")}>red</button>
-        <button type="button" onClick={()=>setColor("green")}>green</button>
+        <button id="btn1" type="button" onClick={()=>setColor("blue")} style={styles.btn1}>blue</button>
+        <button id="btn2" type="button" onClick={()=>setColor("red")} style={styles.btn2}>red</button>
+        <button id="btn3" type="button" onClick={()=>setColor("green")} style={styles.btn3}>green</button>
         </>
     );
 
